d286d86e2bb0ca81f4512960e449c5f0de0c5f61
CONSTANT“ <constantbourgois@gmail.com>
2018-01-24 14:47:16 +0100

    beso

--- js/script.js
+++ js/script.js
@@ -1,38 +1,24 @@
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    animationLoop: false,
-    itemWidth: 210,
+$(document).ready(function () {
 
-  });
-});
+	$('.flexslider').flexslider({
+		animation: "slide",
+		animationLoop: false,
+		itemWidth: 210,
 
-
-jQuery(document).ready(function() {
-    jQuery('.post').addClass("hidden").viewportChecker({
-        classToAdd: 'animated fadeInUp',
-        offset: 100
-       });
-});
-
-jQuery(document).ready(function() {
-    jQuery('.postleft').addClass("hidden").viewportChecker({
-        classToAdd: 'animated slideInLeft',
-        offset: 100
-       });
-});
-
-jQuery(document).ready(function() {
-    jQuery('.postright').addClass("hidden").viewportChecker({
-        classToAdd: 'animated slideInRight',
-        offset: 100
-       });
-});
-
-
-$(document).ready(function(){
-    $('.handle').on('click',function(){
-        $('.mobilenav ul').toggleClass('show');
-    });
-    
+	});
+	$('.post').addClass("hidden").viewportChecker({
+		classToAdd: 'animated fadeInUp',
+		offset: 100
+	});
+	$('.postleft').addClass("hidden").viewportChecker({
+		classToAdd: 'animated slideInLeft',
+		offset: 100
+	});
+	$('.postright').addClass("hidden").viewportChecker({
+		classToAdd: 'animated slideInRight',
+		offset: 100
+	});
+	$('.handle').on('click', function () {
+		$('.mobilenav ul').toggleClass('show');
+	});
 });
